52655e9e14f02315da14681225b271a42e62a1e3
Dmitry <temere@gmail.com>
2021-08-18 20:17:50 +0200

    Update orders field

--- public/app.js
+++ public/app.js
@@ -16,7 +16,13 @@ function add_to_cart(id) {
     x = x * 1 + 1;
     window.localStorage.setItem(key, x);
 
-    alert('Items in your cart: ' + cart_get_number_of_items());
+    update_orders_input();
+}
+
+function update_orders_input()
+{
+    var orders = cart_get_orders();
+    $('#orders_input').val(orders);
 }
 
 function cart_get_number_of_items(){
@@ -33,4 +39,20 @@ function cart_get_number_of_items(){
         }
     }
     return cnt;
+}
+
+function cart_get_orders(){
+    var orders = '';
+    
+    for (var i = 0; i < window.localStorage.length; i++)
+    {
+        var key = window.localStorage.key(i);
+        var value = window.localStorage.getItem(key);
+
+        if (key.indexOf('product_') == 0)
+        {
+            orders = orders + key + '=' + value + ',';
+        }
+    }
+    return orders;
 }
